b675605f8f67a8a20f2f25da45a2b223ed90989d
lambdaphoenix <184911647+lambdaphoenix@users.noreply.github.com>
2025-06-19 01:25:37 +0200

    docs: add package-info

---
+++ src/main/java/io/github/_6mal7/advancementLib/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * Provides a fluent API for registering and managing custom advancements in PaperMC plugins.
+ *
+ * <p>This package contains the main entry point, builder classes, and utility interfaces for
+ * event-driven advancement logic.
+ *
+ * <p><b>Package Specification:</b> For detailed design and usage, see the <a
+ * href="https://github.com/6mal7/AdvancementLib/blob/main/README.md">README</a>.
+ *
+ * @author 6mal7
+ * @since 0.1.0
+ * @version 0.2.1
+ * @see io.github._6mal7.advancementLib.AdvancementAPI
+ * @see <a href="https://6mal7.github.io/AdvancementLib/javadoc/">Project Javadoc</a>
+ */
+package io.github._6mal7.advancementLib;
